b1e59e388c02792e1dcf2880664951f595ef4dbd
Charlie Sergeant <sergeach@kean.edu>
2023-07-30 19:45:52 -0400

    Removes plotting and makes data file used the local reference

--- process.py
+++ process.py
@@ -170,16 +170,13 @@ def train_test_split_df(df, test_size=0.2, random_state=None, even=False):
     return X_train, X_test, y_train, y_test
 
 
-def fit_shap(X, y, save_plot=False, save_path=None):
+def fit_shap(X, y):
     """
     Train a Machine Learning Model (XGBoost) and compute SHAP values for feature importance.
 
     Parameters:
         X (pd.DataFrame): The feature matrix.
         y (pd.Series): The target variable.
-        visualize (bool, optional): If True, create a feature importance plot (Summary Plot).
-        save_plot (bool, optional): If True, save the plot instead of displaying it.
-        save_path (str, optional): The path to save the plot. Required if save_plot is True.
 
     Returns:
         xgb.XGBClassifier: The trained XGBoost model.
@@ -193,17 +190,15 @@ def fit_shap(X, y, save_plot=False, save_path=None):
 
     average_shap_values = pd.DataFrame(shap_values).mean()
     shap.summary_plot(shap_values, X, plot_type="bar", feature_names=X.columns)
-    if save_plot and save_path:
-        plt.savefig(save_path,format='png')  # Save the plot to the specified path
     return model, explainer
 
 
-def run(disallowed_features=None, save_plot=False, save_path=''):
+def run(disallowed_features=None):
     """
     Load data, transform it, and train models with and without stratified train-test split.
 
     """
-    df = pd.read_csv('https://github.com/CharlieSergeant/goto-project/raw/main/data/raw/Sample%20Data%20for%20Interview%20Project.csv')
+    df = pd.read_csv('./data/raw/Sample Data for Interview Project.csv')
     transformed_df = transforms(df)
 
     if disallowed_features is not None:
@@ -213,13 +208,13 @@ def run(disallowed_features=None, save_plot=False, save_path=''):
 
     print('Running Unstratified fit_shap test...')
     unstratified_X_train, unstratified_X_test, unstratified_y_train, unstratified_y_test = train_test_split_df(transformed_df, test_size=0.2, random_state=42, even=False)
-    unstratified_train_model, unstratified_shap = fit_shap(unstratified_X_train, unstratified_y_train, save_plot=save_plot, save_path=f'{save_path}unstratified_shap_chart.png')
+    unstratified_train_model, unstratified_shap = fit_shap(unstratified_X_train, unstratified_y_train)
     unstratified_accuracy = unstratified_train_model.score(unstratified_X_test, unstratified_y_test)
     print("Unstratified Model Accuracy:", unstratified_accuracy)
 
     print('Running Stratified fit_shap test...')
     stratified_X_train, stratified_X_test, stratified_y_train, stratified_y_test = train_test_split_df(transformed_df, test_size=0.2, random_state=42, even=True)
-    stratified_train_model, stratified_shap = fit_shap(stratified_X_train, stratified_y_train, save_plot=save_plot, save_path=f'{save_path}stratified_shap_chart.png')
+    stratified_train_model, stratified_shap = fit_shap(stratified_X_train, stratified_y_train)
     stratified_accuracy = stratified_train_model.score(stratified_X_test, stratified_y_test)
     print("Stratified Model Accuracy:", stratified_accuracy)
 
@@ -227,6 +222,4 @@ def run(disallowed_features=None, save_plot=False, save_path=''):
 if __name__ == "__main__":
     run(
         disallowed_features=None,
-        save_plot=True,
-        save_path='./data/output/'
     )
